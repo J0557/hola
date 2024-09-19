cd6e1fc4ffff417b7f3dc9a501c58698ca59b971
J0557 <tirado.perez.joselino@gmail.com>
2024-09-18 20:57:00 -0600

    18/09/2024 Tirado Perez Joselino

---
+++ script.js
@@ -0,0 +1,37 @@
+alert(hola);
+
+/*
+comentarios multilinea
+*/
+
+//Variables
+
+//Declaracion Variables
+
+let nombre = "Jos";
+const PI = 3.141592;
+var edad = 17;
+
+/*Tipos de Datos
+
+Cadena de texto (string): "Hola mundo";
+Numero (number): 3 , 2 , 1;
+Boleano (bolean): true, false
+*/
+
+//Ejemplo
+
+let mensaje = "Hola mundo";
+let numero = 17;
+let Estudiante = true;
+
+//Operadoes Matematicos
+let a = 5;
+let b = 2;
+
+let suma = a+b;
+let resta = a-b;
+let multiplicacion = a*b;
+let division = a/b;
+
+//git add. / git commit -m "18/09/2024 Tirado Perez Joselino"
